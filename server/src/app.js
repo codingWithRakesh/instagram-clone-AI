07bf44bfa2f445c03ef47538c76662b04aa1be3b
Tarapada Garai <146563644+codingWithRakesh@users.noreply.github.com>
2025-03-06 01:29:02 +0530

    first commit

---
+++ server/src/app.js
@@ -0,0 +1,29 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+console.log("CORS_ORIGIN:", process.env.CORS_ORIGIN);
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173/",
+    credentials: true
+}))
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(express.static("public"))
+app.use(cookieParser())
+
+// import userRouter from "./routes/user.route.js"
+// import keepNoteRouter from "./routes/keepNote.route.js"
+// import labelRouter from "./routes/label.router.js"
+
+// app.use("/user", userRouter)
+// app.use("/keepnote", keepNoteRouter)
+// app.use("/label", labelRouter)
+
+app.get("/", (req, res) => {
+    res.send("working")
+})
+
+
+export { app }
---
+++ server/src/constants.js
@@ -0,0 +1,6 @@
+export const DB_NAME = "Instagram"
+export const options = {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'None'
+}
