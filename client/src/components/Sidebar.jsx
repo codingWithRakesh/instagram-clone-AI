922fc3333df0ac6225876dda04e6a28aa78bc1cc
Tarapada Garai <146563644+codingWithRakesh@users.noreply.github.com>
2025-03-21 23:36:39 +0530

    update App.css to add left border and improve box-shadow styles for better UI consistency

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -12,30 +12,63 @@ const Sidebar = () => {
   const [isCreatVisible, setIsCreatVisible] = useUpload()
   const [forMessage, setForMessage] = useState(false)
   const [, setMore] = useMore()
+
+  const sidebarClick = (value) => {
+    if (value == "home") {
+      setIsSerachVisible(false)
+      setIsNotoficationVisible(false)
+      setIsCreatVisible(false)
+      setForMessage(false)
+    } else if (value == "search") {
+      setIsSerachVisible((v) => !v)
+      setIsNotoficationVisible(false)
+      setIsCreatVisible(false)
+      setForMessage(false)
+    } else if (value == "explore") {
+      setIsSerachVisible(false)
+      setIsNotoficationVisible(false)
+      setIsCreatVisible(false)
+      setForMessage(false)
+    } else if (value == "reels") {
+      setIsSerachVisible(false)
+      setIsNotoficationVisible(false)
+      setIsCreatVisible(false)
+      setForMessage(false)
+    } else if (value == "messages") {
+      setForMessage(true)
+      setIsSerachVisible(false)
+      setIsNotoficationVisible(false)
+      setIsCreatVisible(false)
+    } else if (value == "notification") {
+      setIsNotoficationVisible((v) => !v)
+      setIsSerachVisible(false)
+      setIsCreatVisible(false)
+      setForMessage(false)
+    } else if (value == "create") {
+      setIsCreatVisible((v) => !v)
+    } else if (value == "profile") {
+      setIsSerachVisible(false)
+      setIsNotoficationVisible(false)
+      setIsCreatVisible(false)
+      setForMessage(false)
+    }
+  }
+
   const sides = [
     {
-      fun: () => {
-        setIsSerachVisible(false)
-        setIsNotoficationVisible(false)
-        setIsCreatVisible(false)
-        setForMessage(false)
-      },
+      fun: () => sidebarClick("home"),
       path: "/",
-      code: <svg aria-label="Home" className="_ab6-" color="rgb(0, 0, 0)" fill="rgb(0, 0, 0)" height="24"
+      code: <svg aria-label="Home" className="_ab6-" color="rgb(0, 0, 0)" fill="rgb(0, 0, 0)" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M9.005 16.545a2.997 2.997 0 0 1 2.997-2.997A2.997 2.997 0 0 1 15 16.545V22h7V11.543L12 2 2 11.543V22h7.005Z" fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth="2"></path></svg>,
+      name: "Home",
+      active: <svg aria-label="Home" className="_ab6-" color="rgb(0, 0, 0)" fill="rgb(0, 0, 0)" height="24"
         role="img" viewBox="0 0 24 24" width="24">
         <path
           d="M22 23h-6.001a1 1 0 0 1-1-1v-5.455a2.997 2.997 0 1 0-5.993 0V22a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V11.543a1.002 1.002 0 0 1 .31-.724l10-9.543a1.001 1.001 0 0 1 1.38 0l10 9.543a1.002 1.002 0 0 1 .31.724V22a1 1 0 0 1-1 1Z">
         </path>
-      </svg>,
-      name: "Home"
+      </svg>
     },
     {
-      fun: () => {
-        setIsSerachVisible((v) => !v)
-        setIsNotoficationVisible(false)
-        setIsCreatVisible(false)
-        setForMessage(false)
-      },
+      fun: () => sidebarClick("search"),
       code:
         <svg aria-label="Search" className="_ab6-" color="rgb(0, 0, 0)" fill="rgb(0, 0, 0)" height="24"
           role="img" viewBox="0 0 24 24" width="24">
@@ -59,15 +92,11 @@ const Sidebar = () => {
             y2="22">
           </line>
         </svg>,
-      name: "Search"
+      name: "Search",
+      active: <svg aria-label="Search" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Search</title><path d="M18.5 10.5a8 8 0 1 1-8-8 8 8 0 0 1 8 8Z" fill="none" stroke="currentColor" stroke-linecap="round" strokeLinejoin="round" strokeWidth="3"></path><line fill="none" stroke="currentColor" stroke-linecap="round" strokeLinejoin="round" strokeWidth="3" x1="16.511" x2="21.643" y1="16.511" y2="21.643"></line></svg>
     },
     {
-      fun: () => {
-        setIsSerachVisible(false)
-        setIsNotoficationVisible(false)
-        setIsCreatVisible(false)
-        setForMessage(false)
-      },
+      fun: () => sidebarClick("explore"),
       path: "/explore",
       code: <svg aria-label="Explore" className="_ab6-" color="rgb(0, 0, 0)" fill="rgb(0, 0, 0)" height="24"
         role="img" viewBox="0 0 24 24" width="24">
@@ -94,15 +123,11 @@ const Sidebar = () => {
           strokeWidth="2">
         </circle>
       </svg>,
-      name: "Explore"
+      name: "Explore",
+      active: <svg aria-label="Explore" className="x1lliihq x1n2onr6" color="rgb(0, 0, 0)" fill="rgb(0, 0, 0)" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Explore</title><path d="m13.173 13.164 1.491-3.829-3.83 1.49ZM12.001.5a11.5 11.5 0 1 0 11.5 11.5A11.513 11.513 0 0 0 12.001.5Zm5.35 7.443-2.478 6.369a1 1 0 0 1-.57.569l-6.36 2.47a1 1 0 0 1-1.294-1.294l2.48-6.369a1 1 0 0 1 .57-.569l6.359-2.47a1 1 0 0 1 1.294 1.294Z"></path></svg>
     },
     {
-      fun: () => {
-        setIsSerachVisible(false)
-        setIsNotoficationVisible(false)
-        setIsCreatVisible(false)
-        setForMessage(false)
-      },
+      fun: () => sidebarClick("reels"),
       path: "/reels",
       code: <svg aria-label="Reels" className="_ab6-" color="rgb(0, 0, 0)" fill="rgb(0, 0, 0)" height="24"
         role="img" viewBox="0 0 24 24" width="24">
@@ -151,15 +176,11 @@ const Sidebar = () => {
           fillRule="evenodd">
         </path>
       </svg>,
-      name: "Reels"
+      name: "Reels",
+      active: <svg aria-label="Reels" className="_ab6-" color="rgb(0, 0, 0)" fill="rgb(0, 0, 0)" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="m12.823 1 2.974 5.002h-5.58l-2.65-4.971c.206-.013.419-.022.642-.027L8.55 1Zm2.327 0h.298c3.06 0 4.468.754 5.64 1.887a6.007 6.007 0 0 1 1.596 2.82l.07.295h-4.629L15.15 1Zm-9.667.377L7.95 6.002H1.244a6.01 6.01 0 0 1 3.942-4.53Zm9.735 12.834-4.545-2.624a.909.909 0 0 0-1.356.668l-.008.12v5.248a.91.91 0 0 0 1.255.84l.109-.053 4.545-2.624a.909.909 0 0 0 .1-1.507l-.1-.068-4.545-2.624Zm-14.2-6.209h21.964l.015.36.003.189v6.899c0 3.061-.755 4.469-1.888 5.64-1.151 1.114-2.5 1.856-5.33 1.909l-.334.003H8.551c-3.06 0-4.467-.755-5.64-1.889-1.114-1.15-1.854-2.498-1.908-5.33L1 15.45V8.551l.003-.189Z" fill-rule="evenodd"></path></svg>
     },
     {
-      fun : () => {
-        setForMessage(true)
-        setIsSerachVisible(false)
-        setIsNotoficationVisible(false)
-        setIsCreatVisible(false)
-      },
+      fun: () => sidebarClick("messages"),
       path: "/direct/inbox",
       code: <svg aria-label="Direct" className="_ab6-" color="rgb(0, 0, 0)" fill="rgb(0, 0, 0)" height="24"
         role="img" viewBox="0 0 24 24" width="24">
@@ -181,15 +202,11 @@ const Sidebar = () => {
           strokeWidth="2">
         </polygon>
       </svg>,
-      name: "Messages"
+      name: "Messages",
+      active: <svg aria-label="Direct" className="_ab6-" color="rgb(0, 0, 0)" fill="rgb(0, 0, 0)" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M22.91 2.388a.69.69 0 0 0-.597-.347l-20.625.002a.687.687 0 0 0-.482 1.178L7.26 9.16a.686.686 0 0 0 .778.128l7.612-3.657a.723.723 0 0 1 .937.248.688.688 0 0 1-.225.932l-7.144 4.52a.69.69 0 0 0-.3.743l2.102 8.692a.687.687 0 0 0 .566.518.655.655 0 0 0 .103.008.686.686 0 0 0 .59-.337L22.903 3.08a.688.688 0 0 0 .007-.692" fill-rule="evenodd"></path></svg>
     },
     {
-      fun: () => {
-        setIsNotoficationVisible((v) => !v)
-        setIsSerachVisible(false)
-        setIsCreatVisible(false)
-        setForMessage(false)
-      },
+      fun: () => sidebarClick("notification"),
       code: <svg aria-label="Notifications" className="x1lliihq x1n2onr6" color="rgb(0, 0, 0)"
         fill="rgb(0, 0, 0)" height="24" role="img" viewBox="0 0 24 24" width="24">
         <title>Notifications</title>
@@ -197,10 +214,11 @@ const Sidebar = () => {
           d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z">
         </path>
       </svg>,
-      name: "Notification"
+      name: "Notification",
+      active: <svg aria-label="Notifications" className="x1lliihq x1n2onr6" color="rgb(0, 0, 0)" fill="rgb(0, 0, 0)" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Notifications</title><path d="M17.075 1.987a5.852 5.852 0 0 0-5.07 2.66l-.008.012-.01-.014a5.878 5.878 0 0 0-5.062-2.658A6.719 6.719 0 0 0 .5 8.952c0 3.514 2.581 5.757 5.077 7.927.302.262.607.527.91.797l1.089.973c2.112 1.89 3.149 2.813 3.642 3.133a1.438 1.438 0 0 0 1.564 0c.472-.306 1.334-1.07 3.755-3.234l.978-.874c.314-.28.631-.555.945-.827 2.478-2.15 5.04-4.372 5.04-7.895a6.719 6.719 0 0 0-6.425-6.965Z"></path></svg>
     },
     {
-      fun: () => setIsCreatVisible((v) => !v),
+      fun: () => sidebarClick("create"),
       code: <svg aria-label="New post" className="_ab6-" color="rgb(0, 0, 0)" fill="rgb(0, 0, 0)" height="24"
         role="img" viewBox="0 0 24 24" width="24">
         <path
@@ -218,12 +236,7 @@ const Sidebar = () => {
       name: "Create"
     },
     {
-      fun: () => {
-        setIsSerachVisible(false)
-        setIsNotoficationVisible(false)
-        setIsCreatVisible(false)
-        setForMessage(false)
-      },
+      fun: () => sidebarClick("profile"),
       path: "/tarapada_9065",
       code:
         <div className={`imgPro ${(forMessage || isNotoficationVisible || isSerachVisible) ? `marginRight` : ""}`}>
@@ -232,10 +245,6 @@ const Sidebar = () => {
       name: "Profile"
     }
   ]
-
-  useEffect(() => {
-    console.log({ isCreatVisible, isNotoficationVisible, isSerachVisible,forMessage })
-  }, [isCreatVisible, isNotoficationVisible, isSerachVisible,forMessage])
 
 
   return (
@@ -250,13 +259,13 @@ const Sidebar = () => {
               fill="currentColor" fillRule="evenodd"></path>
           </svg>
             :
-          <svg aria-label="Instagram" className="x1lliihq x1n2onr6" color="rgb(0, 0, 0)" fill="rgb(0, 0, 0)"
-            height="24" role="img" viewBox="0 0 24 24" width="24">
-            <title>Instagram</title>
-            <path
-              d="M12 2.982c2.937 0 3.285.011 4.445.064a6.087 6.087 0 0 1 2.042.379 3.408 3.408 0 0 1 1.265.823 3.408 3.408 0 0 1 .823 1.265 6.087 6.087 0 0 1 .379 2.042c.053 1.16.064 1.508.064 4.445s-.011 3.285-.064 4.445a6.087 6.087 0 0 1-.379 2.042 3.643 3.643 0 0 1-2.088 2.088 6.087 6.087 0 0 1-2.042.379c-1.16.053-1.508.064-4.445.064s-3.285-.011-4.445-.064a6.087 6.087 0 0 1-2.043-.379 3.408 3.408 0 0 1-1.264-.823 3.408 3.408 0 0 1-.823-1.265 6.087 6.087 0 0 1-.379-2.042c-.053-1.16-.064-1.508-.064-4.445s.011-3.285.064-4.445a6.087 6.087 0 0 1 .379-2.042 3.408 3.408 0 0 1 .823-1.265 3.408 3.408 0 0 1 1.265-.823 6.087 6.087 0 0 1 2.042-.379c1.16-.053 1.508-.064 4.445-.064M12 1c-2.987 0-3.362.013-4.535.066a8.074 8.074 0 0 0-2.67.511 5.392 5.392 0 0 0-1.949 1.27 5.392 5.392 0 0 0-1.269 1.948 8.074 8.074 0 0 0-.51 2.67C1.012 8.638 1 9.013 1 12s.013 3.362.066 4.535a8.074 8.074 0 0 0 .511 2.67 5.392 5.392 0 0 0 1.27 1.949 5.392 5.392 0 0 0 1.948 1.269 8.074 8.074 0 0 0 2.67.51C8.638 22.988 9.013 23 12 23s3.362-.013 4.535-.066a8.074 8.074 0 0 0 2.67-.511 5.625 5.625 0 0 0 3.218-3.218 8.074 8.074 0 0 0 .51-2.67C22.988 15.362 23 14.987 23 12s-.013-3.362-.066-4.535a8.074 8.074 0 0 0-.511-2.67 5.392 5.392 0 0 0-1.27-1.949 5.392 5.392 0 0 0-1.948-1.269 8.074 8.074 0 0 0-2.67-.51C15.362 1.012 14.987 1 12 1Zm0 5.351A5.649 5.649 0 1 0 17.649 12 5.649 5.649 0 0 0 12 6.351Zm0 9.316A3.667 3.667 0 1 1 15.667 12 3.667 3.667 0 0 1 12 15.667Zm5.872-10.859a1.32 1.32 0 1 0 1.32 1.32 1.32 1.32 0 0 0-1.32-1.32Z">
-            </path>
-          </svg>}
+            <svg aria-label="Instagram" className="x1lliihq x1n2onr6" color="rgb(0, 0, 0)" fill="rgb(0, 0, 0)"
+              height="24" role="img" viewBox="0 0 24 24" width="24">
+              <title>Instagram</title>
+              <path
+                d="M12 2.982c2.937 0 3.285.011 4.445.064a6.087 6.087 0 0 1 2.042.379 3.408 3.408 0 0 1 1.265.823 3.408 3.408 0 0 1 .823 1.265 6.087 6.087 0 0 1 .379 2.042c.053 1.16.064 1.508.064 4.445s-.011 3.285-.064 4.445a6.087 6.087 0 0 1-.379 2.042 3.643 3.643 0 0 1-2.088 2.088 6.087 6.087 0 0 1-2.042.379c-1.16.053-1.508.064-4.445.064s-3.285-.011-4.445-.064a6.087 6.087 0 0 1-2.043-.379 3.408 3.408 0 0 1-1.264-.823 3.408 3.408 0 0 1-.823-1.265 6.087 6.087 0 0 1-.379-2.042c-.053-1.16-.064-1.508-.064-4.445s.011-3.285.064-4.445a6.087 6.087 0 0 1 .379-2.042 3.408 3.408 0 0 1 .823-1.265 3.408 3.408 0 0 1 1.265-.823 6.087 6.087 0 0 1 2.042-.379c1.16-.053 1.508-.064 4.445-.064M12 1c-2.987 0-3.362.013-4.535.066a8.074 8.074 0 0 0-2.67.511 5.392 5.392 0 0 0-1.949 1.27 5.392 5.392 0 0 0-1.269 1.948 8.074 8.074 0 0 0-.51 2.67C1.012 8.638 1 9.013 1 12s.013 3.362.066 4.535a8.074 8.074 0 0 0 .511 2.67 5.392 5.392 0 0 0 1.27 1.949 5.392 5.392 0 0 0 1.948 1.269 8.074 8.074 0 0 0 2.67.51C8.638 22.988 9.013 23 12 23s3.362-.013 4.535-.066a8.074 8.074 0 0 0 2.67-.511 5.625 5.625 0 0 0 3.218-3.218 8.074 8.074 0 0 0 .51-2.67C22.988 15.362 23 14.987 23 12s-.013-3.362-.066-4.535a8.074 8.074 0 0 0-.511-2.67 5.392 5.392 0 0 0-1.27-1.949 5.392 5.392 0 0 0-1.948-1.269 8.074 8.074 0 0 0-2.67-.51C15.362 1.012 14.987 1 12 1Zm0 5.351A5.649 5.649 0 1 0 17.649 12 5.649 5.649 0 0 0 12 6.351Zm0 9.316A3.667 3.667 0 1 1 15.667 12 3.667 3.667 0 0 1 12 15.667Zm5.872-10.859a1.32 1.32 0 1 0 1.32 1.32 1.32 1.32 0 0 0-1.32-1.32Z">
+              </path>
+            </svg>}
 
         </div>
         <div className="navigason">
@@ -276,7 +285,7 @@ const Sidebar = () => {
             ))
           }
         </div>
-        <div className="more" onClick={()=>setMore((v)=>!v)}>
+        <div className="more" onClick={() => setMore((v) => !v)}>
           <div className="xyasb">
             <i className="fa fa-bars"></i>
             {!(forMessage || isNotoficationVisible || isSerachVisible) && <span>More</span>}
