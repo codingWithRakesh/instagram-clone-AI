e4f8034d4c327f0f4ba4b514990e2808fe2fcbbf
Tarapada Garai <146563644+codingWithRakesh@users.noreply.github.com>
2025-06-07 00:10:42 +0530

    fix: Add missing semicolon in logout route definition

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -23,7 +23,7 @@ const router = Router()
 router.route("/register").post(register);
 router.route("/verfiyEmail").post(verfiyEmail);
 router.route("/login").post(login);
-router.route("/logout").get(verifyLogin, logout)
+router.route("/logout").get(verifyLogin, logout);
 router.route("/updateProfile").patch(verifyLogin, updateProfile)
 router.route("/updateProfileImage").patch(verifyLogin, upload.single("profileImg"), updateProfileImage)
 router.route("/deleteProfileImage").delete(verifyLogin, deleteProfileImage)
